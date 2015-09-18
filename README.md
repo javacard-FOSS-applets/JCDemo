891119b62111e2d6e52fb738240d3fd805391219
leo <leowenyang@163.com>
2015-09-19 06:55:46 +0800

    Simple the Compiler way in order to maintain
    1. fixed Compiler error
    2. add 'How to compiler' in README
    3. Simple compiler way

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 #JCDmo
+
+## how to compiler JC Applet
+1. create *dist* fold at the same level with *src*
+2. get Tool/GeneralJavaApplet.7z and unzip. put it your C:
+3. run Compile_Build_Sun.bat
+4. will get cap file in  
+
+if want to change GeneralJavaApplet fold, you should
+1. modify *JavaCard_kit* in Compile_Build_Sun.bat
+2. modify *JAVA_HOME* in Compile_Build_Sun.bat
+3. modify *-exportpath* in $build.opt
--- jcrandom/Compile_Build_Sun.bat
+++ jcrandom/Compile_Build_Sun.bat
@@ -1,13 +1,11 @@
-SET Package=com\jcdemo\jcdes
-SET Project=jcdes
-REM SET Lib_Path=C:\GeneralJavaApplet\LibGeneration
-SET Lib_Path=E:\GeneralJavaApplet\java_card_kit-2_2_2\lib\api.jar;E:\GeneralJavaApplet\java_card_kit-2_2_2\lib\installer.jar
-SET JavaCard_Kit=E:\GeneralJavaApplet\java_card_kit-2_2_2\lib
-SET JAVA_HOME=E:\GeneralJavaApplet\jdk1.5.0_16
+SET Package=com\jcdemo\jcrandom
+SET Project=jcrandom
+SET JavaCard_Kit=C:\GeneralJavaApplet\java_card_kit-2_2_2\lib
+SET JAVA_HOME=C:\GeneralJavaApplet\jdk1.5.0_16
 
 DEL .\dist\%Package%\*.class
 
-%JAVA_HOME%\bin\javac -g -classpath %Lib_Path% .\src\*.java -d .\dist
+%JAVA_HOME%\bin\javac -g -classpath %JavaCard_Kit%\api.jar;%JavaCard_Kit%installer.jar .\src\*.java -d .\dist
 %JAVA_HOME%\bin\java -classpath %JavaCard_Kit%\offcardverifier.jar;%JavaCard_Kit%\converter.jar; com.sun.javacard.converter.Converter -config $build.opt
 %JAVA_HOME%\bin\java -classpath %JavaCard_Kit%\offcardverifier.jar;%JavaCard_Kit%\scriptgen.jar; com.sun.javacard.scriptgen.Main -o dist\%Package%\javacard\%Project%.scr dist\%Package%\javacard\%Project%.cap
 
